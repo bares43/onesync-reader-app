9de05d8f61e9a04c4d6f522b1d68725447b2d8b2
Jan Bareš <janbares43@gmail.com>
2017-07-23 19:38:25 +0200

    #19 - Načtení HTML šablony ze souboru

---
+++ EbookReader/EbookReader.Droid/DependencyService/AndroidAssetsManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using EbookReader.DependencyService;
+using EbookReader.Droid.DependencyService;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AndroidAssetsManager))]
+namespace EbookReader.Droid.DependencyService {
+    public class AndroidAssetsManager : IAssetsManager {
+
+        public async Task<string> GetFileContentAsync(string filename) {
+            var assetsPath = string.Format(@"Assets\{0}", filename);
+
+            var content = string.Empty;
+
+            return await Task.Run(() => {
+                var file = Application.Context.Assets.Open(assetsPath, Access.Buffer);
+                using (var sr = new StreamReader(file)) {
+                    content = sr.ReadToEnd();
+                }
+
+                return content;
+            });
+
+        }
+    }
+}
