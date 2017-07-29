4cb44e8dfb84c7792104e9e4402ad42fc4345f4e
Jan Bareš <janbares43@gmail.com>
2017-07-29 15:20:47 +0200

    #17 - Načtení epubu

--- EbookReader/EbookReader/App.cs
+++ EbookReader/EbookReader/App.cs
@@ -13,7 +13,6 @@ namespace EbookReader {
     public class App : Application {
 
         FormsWebView webView;
-        Label info;
 
         public App() {
 
@@ -21,11 +20,6 @@ namespace EbookReader {
                 ContentType = Xam.Plugin.Abstractions.Enumerations.WebViewContentType.StringData,
                 WidthRequest = 500,
                 HeightRequest = 500
-            };
-
-            info = new Label {
-                WidthRequest = 500,
-                HeightRequest = 50
             };
 
             var loadButton = new Button {
@@ -50,7 +44,6 @@ namespace EbookReader {
                     VerticalOptions = LayoutOptions.Center,
                     Children = {
                         loadButton,
-                        info,
                         webView
                     }
                 }
@@ -69,31 +62,8 @@ namespace EbookReader {
             var loader = new EpubLoader();
             var epub = await loader.GetEpub(pickedFile.FileName, pickedFile.DataArray);
 
-            info.Text = string.Format("Název: {0}, Autor: {1}\n Popis: {2}", epub.Title, epub.Author, epub.Description);
-        }
-                
-        private string CreateData() {
-            return @"
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Integer lacinia. Aenean vel massa quis mauris vehicula lacinia. Fusce tellus odio, dapibus id fermentum quis, suscipit id erat. Nulla est. Pellentesque arcu. Praesent in mauris eu tortor porttitor accumsan. Donec quis nibh at felis congue commodo. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Duis viverra diam non justo. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Nullam rhoncus aliquam metus. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Proin in tellus sit amet nibh dignissim sagittis. Duis viverra diam non justo. Curabitur bibendum justo non orci. Suspendisse nisl.</p>
-
-<p>Vivamus porttitor turpis ac leo. Maecenas sollicitudin. Aenean vel massa quis mauris vehicula lacinia. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Aenean fermentum risus id tortor. Nullam faucibus mi quis velit. Fusce tellus odio, dapibus id fermentum quis, suscipit id erat. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Duis viverra diam non justo. Integer in sapien. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nulla pulvinar eleifend sem. Quisque porta. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-
-<p>Suspendisse nisl. Morbi scelerisque luctus velit. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Aliquam erat volutpat. Aenean fermentum risus id tortor. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Proin mattis lacinia justo. Pellentesque pretium lectus id turpis. Nulla pulvinar eleifend sem. Pellentesque pretium lectus id turpis. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Praesent id justo in neque elementum ultrices. Quisque tincidunt scelerisque libero. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Pellentesque sapien. Etiam commodo dui eget wisi. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Vivamus porttitor turpis ac leo. Fusce aliquam vestibulum ipsum.</p>
-
-<p>Integer malesuada. Aenean vel massa quis mauris vehicula lacinia. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Mauris tincidunt sem sed arcu. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Aliquam erat volutpat. Etiam egestas wisi a erat. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Nullam faucibus mi quis velit. Donec iaculis gravida nulla. Fusce wisi. Nulla non arcu lacinia neque faucibus fringilla. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim.</p>
-
-<p>In dapibus augue non sapien. Pellentesque sapien. Aenean vel massa quis mauris vehicula lacinia. In rutrum. Duis risus. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Integer vulputate sem a nibh rutrum consequat. Phasellus faucibus molestie nisl. Aenean fermentum risus id tortor. Duis condimentum augue id magna semper rutrum. Proin in tellus sit amet nibh dignissim sagittis. Fusce nibh. Praesent dapibus. Cras pede libero, dapibus nec, pretium sit amet, tempor quis.</p>
-
-<p>Donec vitae arcu. Integer pellentesque quam vel velit. Curabitur bibendum justo non orci. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Aliquam ornare wisi eu metus. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Integer malesuada. Donec vitae arcu. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Nulla est. Integer malesuada. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero.</p>
-
-<p>Nunc tincidunt ante vitae massa. Duis pulvinar. Aenean fermentum risus id tortor. Donec vitae arcu. In convallis. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Aliquam erat volutpat. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Sed ac dolor sit amet purus malesuada congue. Nam sed tellus id magna elementum tincidunt. Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Etiam quis quam.</p>
-
-<p>Fusce tellus. In enim a arcu imperdiet malesuada. Nam sed tellus id magna elementum tincidunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Integer lacinia. Pellentesque ipsum. Suspendisse nisl. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Maecenas aliquet accumsan leo. Sed ac dolor sit amet purus malesuada congue. Nam sed tellus id magna elementum tincidunt.</p>
-
-<p>Mauris dictum facilisis augue. Duis viverra diam non justo. Aliquam erat volutpat. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Nulla est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis risus. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Integer tempor. Nullam faucibus mi quis velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean placerat. Pellentesque ipsum. Aliquam erat volutpat. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Vestibulum fermentum tortor id mi.</p>
-
-<p>Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Nulla est. Nullam sit amet magna in magna gravida vehicula. Integer imperdiet lectus quis justo. Nullam rhoncus aliquam metus. Phasellus et lorem id felis nonummy placerat. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Mauris elementum mauris vitae tortor. Fusce tellus. Aliquam ornare wisi eu metus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est.</p>
-";   
+            var html = string.Format("<h1>{0}</h1><i>{1}</i><p>{2}</p>", epub.Title, epub.Author, epub.Description);
+            this.SendHtml(html);
         }
 
         private void SendHtml(string html) {
@@ -118,7 +88,6 @@ namespace EbookReader {
 
         private void WebView_OnContentLoaded(Xam.Plugin.Abstractions.Events.Inbound.ContentLoadedDelegate eventObj) {
             eventObj.Sender.InjectJavascript("init()");
-            this.SendHtml(this.CreateData());
         }
 
         protected override void OnStart() {
--- EbookReader/EbookReader/Service/EpubLoader.cs
+++ EbookReader/EbookReader/Service/EpubLoader.cs
@@ -15,8 +15,10 @@ namespace EbookReader.Service {
             var folder = await this.LoadEpub(filename, filedata);
 
             var epubFolder = await FileSystem.Current.LocalStorage.GetFolderAsync(folder);
-            var oepbsFolder = await epubFolder.GetFolderAsync("OEBPS");
-            var contentFile = await oepbsFolder.GetFileAsync("content.opf");
+
+            var contentFilePath = await GetContentFilePath(epubFolder);
+
+            var contentFile = await this.OpenFile(contentFilePath, epubFolder);
             var contentFileData = await contentFile.ReadAllTextAsync();
 
             var xml = XDocument.Parse(contentFileData);
@@ -32,12 +34,47 @@ namespace EbookReader.Service {
             return epub;
         }
 
+        private async Task<string> GetContentFilePath(IFolder epubFolder) {
+            var containerFile = await this.OpenFile("META-INF/container.xml", epubFolder);
+            var containerFileContent = await containerFile.ReadAllTextAsync();
+            var xmlContainer = XDocument.Parse(containerFileContent);
+            var contentFilePath = xmlContainer.Root
+                .Descendants()
+                .First(o => o.Name.LocalName == "rootfiles")
+                .Descendants()
+                .First(o => o.Name.LocalName == "rootfile")
+                .Attributes()
+                .First(o => o.Name.LocalName == "full-path")
+                .Value;
+            return contentFilePath;
+        }
+
+        private async Task<IFile> OpenFile(string name, IFolder folder) {
+            folder = await this.GetFileFolder(name, folder);
+            return await folder.GetFileAsync(this.GetFileName(name));
+        }
+
+        private async Task<IFolder> GetFileFolder(string name, IFolder folder) {
+            while (name.Contains("/")) {
+                var path = name.Split(new char[] { '/' }, 2);
+                var folderName = path[0];
+                name = path[1];
+                folder = await folder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+            }
+
+            return folder;
+        }
+
+        private string GetFileName(string path) {
+            return path.Split('/').Last();
+        }
+
         private async Task<string> LoadEpub(string filename, byte[] filedata) {
             var folderName = filename.Split('.').First();
 
-            IFolder rootFolder = FileSystem.Current.LocalStorage;
-            IFolder folder = await rootFolder.CreateFolderAsync(folderName, CreationCollisionOption.ReplaceExisting);
-            IFile file = await folder.CreateFileAsync("temp.zip", CreationCollisionOption.OpenIfExists);
+            var rootFolder = FileSystem.Current.LocalStorage;
+            var folder = await rootFolder.CreateFolderAsync(folderName, CreationCollisionOption.ReplaceExisting);
+            var file = await folder.CreateFileAsync("temp.zip", CreationCollisionOption.OpenIfExists);
 
             using (Stream stream = await file.OpenAsync(FileAccess.ReadAndWrite)) {
                 await stream.WriteAsync(filedata, 0, filedata.Length);
@@ -47,27 +84,16 @@ namespace EbookReader.Service {
                         if (zipEntry.IsFile) {
                             var zipEntryStream = zf.GetInputStream(zipEntry);
 
-                            var name = zipEntry.Name;
+                            var name = this.GetFileName(zipEntry.Name);
 
-                            var fileFolder = folder;
-
-                            while (name.Contains("/")) {
-                                var parts = name.Split(new char[] { '/' }, 2);
-                                var currentFolderName = parts[0];
-                                name = parts[1];
-
-                                fileFolder = await folder.CreateFolderAsync(currentFolderName, CreationCollisionOption.OpenIfExists);
-                            }
-
+                            var fileFolder = await this.GetFileFolder(zipEntry.Name, folder);
+                            
                             IFile zipEntryFile = await fileFolder.CreateFileAsync(name, CreationCollisionOption.OpenIfExists);
                             var str = zf.GetInputStream(zipEntry);
                             using (Stream outPutFileStream = await zipEntryFile.OpenAsync(FileAccess.ReadAndWrite)) {
                                 await str.CopyToAsync(outPutFileStream);
                             }
                         }
-
-                       
-
                     }
                 }
             }
