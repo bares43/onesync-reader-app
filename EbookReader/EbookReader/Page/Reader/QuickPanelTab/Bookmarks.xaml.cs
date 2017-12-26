92545db3a272e7ff6798209dd3d3c6c612ef8ed5
Jan Bareš <janbares43@gmail.com>
2017-12-26 12:01:20 +0100

    #34 - Záložky

--- EbookReader/EbookReader/Page/Reader/QuickPanelTab/Bookmarks.xaml.cs
+++ EbookReader/EbookReader/Page/Reader/QuickPanelTab/Bookmarks.xaml.cs
@@ -39,98 +39,10 @@ namespace EbookReader.Page.Reader.QuickPanelTab {
             var layouts = new List<StackLayout>();
 
             foreach (var item in items.OrderBy(o => o.Position.Spine).ThenBy(o => o.Position.SpinePosition)) {
-                var stackLayout = new StackLayout {
-                    Orientation = StackOrientation.Horizontal,
-                };
-
-                var previewMode = new StackLayout {
-                    Orientation = StackOrientation.Horizontal,
-                };
-
-                var editMode = new StackLayout {
-                    IsVisible = false,
-                    Orientation = StackOrientation.Horizontal,
-                    HorizontalOptions = LayoutOptions.FillAndExpand,
-                };
-
-                var deleteIcon = new Image {
-                    WidthRequest = 26,
-                    HeightRequest = 26,
-                    Source = "delete.png",
-                };
-
-                var deleteIconTgr = new TapGestureRecognizer();
-                deleteIconTgr.Tapped += (s, e) => this.DeleteBookmark(item);
-                deleteIcon.GestureRecognizers.Add(deleteIconTgr);
-
-                var entry = new Entry {
-                    Text = item.Name,
-                    HorizontalOptions = LayoutOptions.FillAndExpand,
-                };
-
-                // preview
-                var editIcon = new Image {
-                    WidthRequest = 26,
-                    HeightRequest = 26,
-                    Source = "edit.png",
-                };
-
-                var editIconTgr = new TapGestureRecognizer();
-                editIconTgr.Tapped += (s, e) => {
-                    editMode.IsVisible = true;
-                    previewMode.IsVisible = false;
-                    entry.Focus();
-                };
-                editIcon.GestureRecognizers.Add(editIconTgr);
-
-                var label = new Label {
-                    Text = item.Name,
-                    FontSize = Device.GetNamedSize(Device.RuntimePlatform == Device.Android ? NamedSize.Large : NamedSize.Medium, typeof(Label))
-                };
-
-                var tgr = new TapGestureRecognizer();
-                tgr.Tapped += (s, e) => this.ClickToItem(item);
-                label.GestureRecognizers.Add(tgr);
-
-                previewMode.Children.Add(editIcon);
-                previewMode.Children.Add(label);
-
-                // edit
-                var saveIcon = new Image {
-                    WidthRequest = 26,
-                    HeightRequest = 26,
-                    Source = "save.png",
-                };
-
-                var saveIconTgr = new TapGestureRecognizer();
-                saveIconTgr.Tapped += (s, e) => {
-                    previewMode.IsVisible = true;
-                    editMode.IsVisible = false;
-                    item.Name = entry.Text;
-                    IocManager.Container.Resolve<IMessageBus>().Send(new ChangedBookmarkName { Bookmark = item });
-                };
-                saveIcon.GestureRecognizers.Add(saveIconTgr);
-
-                editMode.Children.Add(saveIcon);
-                editMode.Children.Add(entry);
-
-                stackLayout.Children.Add(deleteIcon);
-                stackLayout.Children.Add(editMode);
-                stackLayout.Children.Add(previewMode);
-
-                layouts.Add(stackLayout);
+                layouts.Add(new BookmarksTab.Bookmark(item));
             }
 
             return layouts;
         }
-
-        private void ClickToItem(Model.Bookshelf.Bookmark item) {
-            IocManager.Container.Resolve<IMessageBus>().Send(new OpenBookmark { Bookmark = item });
-        }
-
-        private void DeleteBookmark(Model.Bookshelf.Bookmark item) {
-            IocManager.Container.Resolve<IMessageBus>().Send(new DeleteBookmark { Bookmark = item });
-        }
-
     }
 }
